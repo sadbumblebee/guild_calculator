76338cf854f6e0a08b33f2a7033c5900a3a83b3c
Kati <katherine.f235@gmail.com>
2023-07-26 21:09:58 -0400

    Styling fixes to component

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -38,49 +38,134 @@
 </script>
 
 <p>What's your job category?</p>
-<Select {items} bind:value={jobType} />
+<Select
+    {items}
+    bind:value={jobType}
+    class='dropdown'
+    --placeholder-color='rgb(117, 117, 117)'
+    inputStyles="font-family: BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif;"
+/>
 <p>How much do you currently make?</p>
 <div class="salary-input-container">
     <!-- This bind is how we handle binding a prop/value -->
-    <input bind:value={salaryRaw} placeholder="Enter your salary" />
+    <input class='text' bind:value={salaryRaw} placeholder="Enter your salary" />
 </div>
 
 <!-- In svelte you use logic blocks like this ... -->
 <!-- IF YOU SELECT A JOB TYPE SHOW BELOW -->
 {#if jobType}
-    <p>As a {jobType.label.toLowerCase()} your minimum would be ...</p>
-    <p><strong>Post: </strong>{formatSalary(postMin)}</p>
-    <p><strong>Guild: </strong>{formatSalary(guildMin)}</p>
+    <p style='text-align: center; border-top: 1px solid rgb(223, 58, 79); margin-top: 24px; padding-top: 24px'>As {jobType.label.charAt(0) === 'A' || jobType.label.charAt(0) === '#' ? 'an' : 'a'} {jobType.label.toLowerCase()} your minimum would be...</p>
+    <div class='salary-min-group'>
+        <div class='salary-min'>
+            <p class='head'>Post:</p>
+            <p class='num'>{formatSalary(postMin)}</p>
+            {#if salary && salary < postMin}
+                <p class='difference-post'>+{formatSalary(postMin - salary)}/year</p>
+            {/if}
+        </div>
+        <div class='salary-min'>
+            <p class='head'>Guild:</p>
+            <p class='num'>{formatSalary(guildMin)}</p>
+            {#if salary && salary < guildMin}
+                <p class='difference'>+{formatSalary(guildMin - salary)}/year!</p>
+            {/if}
+        </div>
+    </div>
+
 {/if}
 <!-- IF YOU ENTERED A SALARY SHOW THE CHANGE -->
 {#if salary && jobType}
-    <p>Salary section</p>
-    <p><strong>{formatSalary(salary)}</strong></p>
+    <!-- <p>Salary section</p> -->
+    <!-- <p>As an employee earning {formatSalary(salary)} a year, you would make <strong>{formatSalary(guildMin - salary)}</strong> more a year under the Guild's proposal. </p> -->
     <!-- SALARY LESS THAN POST MINIMUM -->
     {#if salary < postMin}
-    <p>Under the guild proposal: You would make {formatSalary(guildMin - salary)} more a year.</p>
-    <p>Versus the post's proposal of {formatSalary(postMin - salary)} more a year.</p>
+    <!-- <p>Under the Guild's proposal, you would make <strong>{formatSalary(guildMin - salary)} more</strong> a year.</p>
+    <p>Versus the post's proposal of {formatSalary(postMin - salary)} more a year.</p> -->
     <!-- SALARY LESS THAN GUILD MINIMUM -->
     {:else if salary < guildMin}
-    <p>You would make {formatSalary(guildMin - salary)} more a year.</p>
+    <!-- <p>You would make {formatSalary(guildMin - salary)} more a year.</p> -->
     <!-- SALARY GREATER THAN GUILD MINIMUM -->
     <!-- SHOW INCREASE AT TIER1 // 4.0%-->
     {:else if salary < tier1}
-    <p>You would get a 4.0% raise, which would be {formatSalary(salary * .04)} dollars.</p>
+    <p class='pct-raise'>Under the Guild's proposal, you would get a <span class='highlight'>4.0% raise</span>, which would be <span class='highlight'>{formatSalary(salary * .04)}</span> dollars.</p>
     <!-- SHOW INCREASE AT TIER2 // 3.0%-->
     {:else if salary < tier2}
-    <p>You would get a 3.0% raise, which would be {formatSalary(salary * .03)} dollars.</p>
+    <p class='pct-raise'>Under the Guild's proposal, you would get a <span class='highlight'>3.0% raise</span>, which would be <span class='highlight'>{formatSalary(salary * .03)}</span> dollars.</p>
     <!-- SHOW INCREASE AT TIER3 // 2.5%-->
     {:else if salary < tier3}
-    <p>You would get a 2.5% raise, which would be {formatSalary(salary * .025)} dollars.</p>
+    <p class='pct-raise'>Under the Guild's proposal, you would get a <span class='highlight'>2.5% raise</span>, which would be <span class='highlight'>{formatSalary(salary * .025)}</span> dollars.</p>
     <!-- SHOW INCREASE AT TIER4 // 2.0%-->
     {:else if salary >= tier4}
-    <p>You would get a 2.0% raise, which would be {formatSalary(salary * .02)} dollars.</p>
+    <p class='pct-raise'>Under the Guild's proposal, you would get a <span class='highlight'>2.0% raise</span>, which would be <span class='highlight'>{formatSalary(salary * .02)}</span> dollars.</p>
     {/if}
 {/if}
 
 <!-- Styles go down here -->
 <style lang=scss>
+    p {
+        font-family: BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif;
+    }
+
+    .dropdown {
+        // background-color: red;
+        div {
+            background-color: yellow;
+        }
+    }
+
+    .highlight {
+        background-color: rgb(255, 230, 0);
+        font-weight: 700;
+    }
+
+    .pct-raise {
+        margin-top: 24px;
+    }
+
+    .salary-min-group {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: flex-start;
+
+        @media screen and (max-width: 400px) {
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+        }
+    }
+
+    .salary-min {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-width: 120px;
+        text-align: center;
+
+        .head {
+            padding: 0;
+            margin: 0;
+            text-align: center;
+        }
+        .num {
+            font-weight: 700;
+            padding: 0;
+            margin: 0;
+        }
+        .difference {
+            font-size: .9em;
+            font-weight: 700;
+            background-color: rgb(255, 230, 0);
+            margin-top: 8px;
+        }
+        .difference-post {
+            font-size: .9em;
+            margin-top: 8px;
+        }
+    }
+    input {
+        font-family: BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif;
+    }
     .salary-input-container {
         height: 42px;
         width: 50%;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,29 @@ import Input from '$lib/components/Input.svelte'
 
 <!-- All your HTML goes here -->
 <div class="container">
-<h1>Wage calculator</h1>
-<p>Let's see how much money you could be making if management bargained fairly.</p>
+<h1 class='text'>Compare wage proposals ✨</h1>
+<p class='text'>Let's see how much money you could be making if management bargained fairly.</p>
 <Input/>
 </div>
 <!-- When you're done you can put component specific styling below -->
 
 <style lang="scss">
+    .text {
+        font-family: BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif;
+    }
     .container {
-        background-color: #E5E4E2;
+        border: solid 5px rgb(223, 58, 79);
         max-width: 580px;
         margin: 50px auto 0px;
-        padding: 10px;
+        padding: 40px;
     }
     h1 {
         font-size: 2rem;
         color: black;
+        margin-top: 0;
+        text-align: center;
+        @media screen and (max-width: 583px) {
+            text-align: left;
+        }
     }
 </style>
